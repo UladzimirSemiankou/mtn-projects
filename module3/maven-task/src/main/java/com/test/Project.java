d4018cb7dcd4ac42fecfb163de54977656cd4d08
UladzimirSemiankou <Uladzimir_Semiankou@epam.com>
2016-08-08 16:58:21 +0300

    Update Project.java

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -5,21 +5,11 @@ package com.test;
 class Project {
 //dependency on Apache commons-collections
   private Project() {
-   public static void test{
-     logger.log("test")}
-  
+  }
+   public static void test() {
+     logger.log("test")
+   }
    public static void main(String[] args) {
      logger.log("Hellow MTN");
    }
-  }
-}
-class StringUtils { // Compliant
-
-  private StringUtils() {
-  }
-
-  public static String concatenate(String s1, String s2) {
-    return s1 + s2;
-  }
-
 }
