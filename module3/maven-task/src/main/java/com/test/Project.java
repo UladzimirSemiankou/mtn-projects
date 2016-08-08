8c2d10e2b2c73013f9ee7469d9c52ab7391a4ea4
UladzimirSemiankou <Uladzimir_Semiankou@epam.com>
2016-08-08 16:48:43 +0300

    Update Project.java

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -1,18 +1,15 @@
 package com.test;
 
 //dependency on Apache commons-collections
-//import org.apache.commons.collections.Predicate;
 
 class Project {
 //dependency on Apache commons-collections
-//dependency on Apache commons-collections
-//dependency on Apache commons-collections
   public static void test() {
-    System.out.println("test");
+    logger.log("test");
   }
   
   public static void main(String[] args) {
-    System.out.println("Hellow MTN");
+    logger.log("Hellow MTN");
   }
 
 }
